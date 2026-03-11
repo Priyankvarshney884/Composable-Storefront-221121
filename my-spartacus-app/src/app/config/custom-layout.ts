7929385c57c9e434645fe31bc228b2d628f9f9be
Priyank.varshney@electrolux.com <Priyank.varshney@electrolux.com>
2026-03-11 11:51:33 +0530

    adding custome latyout changes to application.

---
+++ my-spartacus-app/src/app/config/custom-layout.ts
@@ -0,0 +1,86 @@
+import { LayoutConfig } from "@spartacus/storefront";
+
+
+export const CustomLayoutConfig: LayoutConfig = {
+    // from here we can customize the layout of the pages with customelayout which is extenting the spartacus LayoutConfig
+    layoutSlots: {
+        LandingPage2Template: {
+            pageFold: 'Section2B',
+            slots: [
+                'Section1',
+                'Section2A',
+                'Section2B',
+                'Section2C',
+                'Section3',
+                'Section4',
+                'Section5',
+            ],
+        },
+        ContentPage1Template: {
+      slots: ['Section2A', 'Section2B'],
+    },
+    CategoryPageTemplate: {
+      pageFold: 'Section2',
+      slots: ['Section1', 'Section2', 'Section3'],
+    },
+    ProductListPageTemplate: {
+      slots: ['ProductLeftRefinements', 'ProductListSlot'],
+    },
+    ProductGridPageTemplate: {
+      slots: ['ProductLeftRefinements', 'ProductGridSlot'],
+    },
+    SearchResultsListPageTemplate: {
+      slots: [
+        'Section2',
+        'ProductLeftRefinements',
+        'SearchResultsListSlot',
+        'Section4',
+      ],
+    },
+    SearchResultsGridPageTemplate: {
+      slots: [
+        'Section2',
+        'ProductLeftRefinements',
+        'SearchResultsGridSlot',
+        'Section4',
+      ],
+    },
+    ProductDetailsPageTemplate: {
+      lg: {
+        pageFold: 'UpSelling',
+      },
+      pageFold: 'Summary',
+      slots: [
+        'Summary',
+        'UpSelling',
+        'CrossSelling',
+        'Tabs',
+        'PlaceholderContentSlot',
+      ],
+    },
+    CartPageTemplate: {
+      slots: ['TopContent', 'CenterRightContentSlot', 'EmptyCartMiddleContent'],
+    },
+    AccountPageTemplate: {
+      slots: ['BodyContent', 'SideContent'],
+    },
+    LoginPageTemplate: {
+      slots: ['LeftContentSlot', 'RightContentSlot'],
+    },
+    ErrorPageTemplate: {
+      slots: ['TopContent', 'MiddleContent', 'BottomContent'],
+    },
+    OrderConfirmationPageTemplate: {
+      slots: ['BodyContent', 'SideContent'],
+    },
+    MultiStepCheckoutSummaryPageTemplate: {
+      slots: ['TopContent', 'BodyContent', 'SideContent', 'BottomContent'],
+    },
+    CheckoutLoginPageTemplate: {
+      slots: ['RightContentSlot'],
+    },
+    MyAccountViewPageTemplate: {
+      slots: ['LeftContentSlot', 'RightContentSlot'],
+    },
+    }
+}
